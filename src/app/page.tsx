48a649deae2afe1def709740908adf52d9f7ffd9
Evilpixi <pixi@dega.org>
2024-12-15 20:19:11 -0300

    added mock link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,16 @@ import Card from "@components/Card";
 export default function Home()
 {
   return (
-    <div className="flex">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start p-7">
-        <Card make="Toyota"
-          model="Corolla"
-          year={2019}
-          price={"R$ 53.990"}
-          imageUrl="/toyota.webp"
-          km="50.000 km"
-          version="2.0 XEI Flex 16V Aut."
-          city="São Paulo"
-        />
-      </main>
-    </div>
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start p-7">
+      <Card make="Toyota"
+        model="Corolla"
+        year={2019}
+        price={"R$ 53.990"}
+        imageUrl="/toyota.webp"
+        km="50.000 km"
+        version="2.0 XEI Flex 16V Aut."
+        city="São Paulo"
+      />
+    </main>
   );
 }
---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+const API_MOCK_ENDPOINT = "https://ast.prd.karvi.com.ar/challenge/cars/ASST-challenge-01JEVJTR90HVPSS2NRPPG02CJ3.json"
+
+export { API_MOCK_ENDPOINT }

ada2855d5cddc9dfe13cf2edb4b914918723dd80
evilpixi <tatosalvi@gmail.com>
2024-12-15 18:17:42 -0300

    created filter panel

---
+++ src/components/FilterPanel.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface FilterPanelProps
+{
+
+}
+
+const FilterPanel: React.FC<FilterPanelProps> = () =>
+{
+  return (
+    <div>
+      FilterPanel
+    </div>
+  );
+};
+
+export default FilterPanel
